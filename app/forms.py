958f37b4703f94b0df8b9bf1da6bab4253bd50e8
aleks169 <av591955@gmail.com>
2019-02-23 21:56:49 +0200

    добовление шрифтов

--- app/forms.py
+++ app/forms.py
@@ -13,11 +13,13 @@ class ContactForm(forms.ModelForm):
     message = forms.CharField(widget=forms.Textarea(
         attrs={'placeholder': 'Сообщение', 'class':'form-control'}), max_length=100, )
     email = forms.EmailField(max_length=100, widget=forms.TextInput(
-        attrs={'placeholder': 'Ваш телефон','class':'form-control'}))
+        attrs={'placeholder': 'Ваш Email:','class':'form-control'}))
+    phone = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={'placeholder': 'Ваше телефон','class':'form-control'}))
 
 
 
 
     class Meta:
         model = ContactUs
-        fields = ('name', 'email', 'message')
+        fields = ('name', 'email', 'message' ,'phone')
--- app/models.py
+++ app/models.py
@@ -11,6 +11,7 @@ from django.utils import timezone
 class ContactUs(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
+    phone = models.CharField(max_length=100)
     message = models.TextField(max_length=240)
     date = models.DateTimeField(default=timezone.now)
 
